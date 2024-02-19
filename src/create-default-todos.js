19bbbfca7d7536cb6c59daa610a5111b7a078c9f
bradenr402 <bradenr402@gmail.com>
2024-02-19 12:50:56 -0500

    fix default todo creation

--- src/create-default-todos.js
+++ src/create-default-todos.js
@@ -2,29 +2,46 @@ import Todo from './todo';
 import List from './list';
 
 export default function createDefaultTodos() {
-  const allLists = [];
-  const pastTodo = new Todo(
-    'Past todo',
-    'This is my post todo description',
-    new Date(2024, 1, 13),
-    'medium'
-  );
-  const todayTodo = new Todo('Today todo', 'This is my today todo description');
-  const futureTodo = new Todo(
-    'Future todo',
-    'This is my future todo description',
-    new Date(2024, 1, 23),
-    'high'
-  );
+  const pastTodo = new Todo({
+    title: 'Past todo',
+    dueDate: new Date(2024, 1, 13),
+    description: 'This is my post todo description',
+  });
+  const todayTodo = new Todo({
+    title: 'Today todo',
+    dueDate: new Date(),
+    description: 'This is my today todo description',
+  });
+  const futureTodo = new Todo({
+    title: 'Future todo',
+    dueDate: new Date(2024, 1, 23),
+    description: 'This is my future todo description',
+  });
+
+  const pastTodo2 = new Todo({
+    title: 'Past todo',
+    dueDate: new Date(2024, 1, 13),
+    list: 'To Do',
+    description: 'This is my post todo description',
+  });
+  const todayTodo2 = new Todo({
+    title: 'Today todo',
+    dueDate: new Date(),
+    list: 'To Do',
+    description: 'This is my today todo description',
+  });
+  const futureTodo2 = new Todo({
+    title: 'Future todo',
+    dueDate: new Date(2024, 1, 23),
+    list: 'To Do',
+    description: 'This is my future todo description',
+  });
 
   const inboxList = new List('Inbox');
-  inboxList.addTasks(todayTodo, futureTodo, pastTodo);
+  inboxList.addTasks(pastTodo, todayTodo, futureTodo);
 
-  const todoList = new List('Todo list');
-  todoList.addTasks(todayTodo, futureTodo, pastTodo);
+  const todoList = new List('To Do');
+  todoList.addTasks(pastTodo2, todayTodo2, futureTodo2);
 
-  allLists.push(inboxList);
-  allLists.push(todoList);
-
-  return allLists;
+  return [pastTodo, pastTodo2, todayTodo, todayTodo2, futureTodo, futureTodo2];
 }
