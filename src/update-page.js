6d27eddcf182e39bef3f05d4e0e4c81c9f6c89d9
bradenr402 <bradenr402@gmail.com>
2024-02-19 14:24:47 -0500

    refactor updatePage()

--- src/update-page.js
+++ src/update-page.js
@@ -2,10 +2,24 @@ import { addListDropdown, addItemToDropdown } from './setup-page';
 
 function updatePage(newTodo) {
   const allLists = [];
+  updateSideLinks(newTodo, allLists);
 
+  const content = document.querySelector('.content');
+  if (!allLists.includes(newTodo.list)) {
+    addListDropdown(newTodo.list, [newTodo], content);
+  } else {
+    const ul = document.querySelector(`ul.${newTodo.list.split(' ').join('')}`);
+    addItemToDropdown(newTodo, ul, newTodo.list);
+  }
+
+  const main = document.querySelector('body main');
+  main.appendChild(content);
+}
+
+function updateSideLinks(newTodo, allLists) {
   const sideLinks = document.querySelectorAll('.side-links li h3');
   sideLinks.forEach((link) => {
-    if (!allLists.includes(link.textContent) && link.textContent !== '') allLists.push(link.textContent);
+    if (!allLists.includes(link.textContent)) allLists.push(link.textContent);
   });
 
   if (!allLists.includes(newTodo.list)) {
@@ -18,17 +32,6 @@ function updatePage(newTodo) {
     li.appendChild(h3);
     sidebarContainer.appendChild(li);
   }
-
-  const content = document.querySelector('.content');
-  if (!allLists.includes(newTodo.list)) {
-    addListDropdown(newTodo.list, [newTodo], content);
-  } else {
-    const ul = document.querySelector(`ul.${newTodo.list.split(' ').join('')}`);
-    addItemToDropdown(newTodo, ul, newTodo.list);
-  }
-
-  const main = document.querySelector('body main');
-  main.appendChild(content);
 }
 
 function updateFormSelectOptions(newTodo) {
