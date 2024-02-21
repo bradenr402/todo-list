70f742eb6b73f05719ff76afe2ab679131da0985
bradenr402 <bradenr402@gmail.com>
2024-02-21 13:07:14 -0500

    update default todos

--- src/create-default-todos.js
+++ src/create-default-todos.js
@@ -1,43 +1,20 @@
 import { Todo, addTodos } from './todo';
 
 export default function createDefaultTodos() {
-  const pastTodo = new Todo({
-    title: 'Past todo',
-    dueDate: new Date(2024, 1, 13),
-    description: 'This is my post todo description',
-  });
-  const todayTodo = new Todo({
-    title: 'Today todo',
-    dueDate: new Date(),
-    description: 'This is my today todo description',
-  });
-  const futureTodo = new Todo({
-    title: 'Future todo',
-    dueDate: new Date(2024, 1, 23),
-    description: 'This is my future todo description',
+  const inboxTodo1 = new Todo({
+    title: 'Take out trash',
+    dueDate: new Date(2024, 1, 22),
   });
 
-  const pastTodo2 = new Todo({
-    title: 'Past todo',
-    dueDate: new Date(2024, 1, 13),
-    list: 'To Do',
-    description: 'This is my post todo description',
-  });
-  const todayTodo2 = new Todo({
-    title: 'Today todo',
-    dueDate: new Date(),
-    list: 'To Do',
-    description: 'This is my today todo description',
-  });
-  const futureTodo2 = new Todo({
-    title: 'Future todo',
-    dueDate: new Date(2024, 1, 23),
-    list: 'To Do',
-    description: 'This is my future todo description',
+  const inboxTodo2 = new Todo({
+    title: 'Wash dishes',
+    dueDate: new Date(2024, 1, 21, 17, 0, 0),
+    description:
+      'Empty dishwasher, load dirty dishes into dishwasher, rerun clean cycle',
   });
 
   const listOfLists = {};
-  addTodos(listOfLists, pastTodo, pastTodo2, todayTodo, todayTodo2, futureTodo, futureTodo2);
+  addTodos(listOfLists, inboxTodo1, inboxTodo2);
 
-  return {listOfLists, todos: [pastTodo, pastTodo2, todayTodo, todayTodo2, futureTodo, futureTodo2]};
+  return { listOfLists, todos: [inboxTodo1, inboxTodo2] };
 }
