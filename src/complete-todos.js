b50a63bd93c3bf4261be3d30d6a155aa99e8a2bc
bradenr402 <bradenr402@gmail.com>
2024-02-23 10:38:53 -0500

    mark todos as complete with checkbox

--- src/complete-todos.js
+++ src/complete-todos.js
@@ -1,12 +1,21 @@
+import listOfLists from './index';
+import { getTodoById } from './todo';
+
 function updateTodoEventListeners() {
   const todos = document.querySelectorAll('.todo');
   todos.forEach((todo) => {
     const todoId = todo.id;
     const checkbox = document.getElementById(`checkbox-${todoId}`);
 
+    const todoObject = getTodoById(listOfLists, todo.id);
     checkbox.addEventListener('change', function () {
-      if (this.checked) todo.style.textDecoration = 'line-through';
-      else todo.style.textDecoration = 'none';
+      if (this.checked) {
+        todo.style.textDecoration = 'line-through';
+        todoObject.markCompleted();
+      } else {
+        todo.style.textDecoration = 'none';
+        todoObject.markUncompleted();
+      }
     });
   });
 }
