97b1eab6abdefbd1d75895298e928de38ad15d0e
bradenr402 <bradenr402@gmail.com>
2024-02-23 16:33:02 -0500

    update default todos

--- src/create-default-todos.js
+++ src/create-default-todos.js
@@ -1,20 +1,33 @@
 import { Todo, addTodos } from './todo';
 
 export default function createDefaultTodos() {
+  const yesterday = new Date();
+  yesterday.setDate(yesterday.getDate() - 1);
+
+  const today = new Date();
+
+  const tomorrow = new Date();
+  tomorrow.setDate(tomorrow.getDate() + 1);
+
   const inboxTodo1 = new Todo({
     title: 'Take out trash',
-    dueDate: new Date(2024, 1, 22),
+    dueDate: yesterday,
   });
 
   const inboxTodo2 = new Todo({
     title: 'Wash dishes',
-    dueDate: new Date(2024, 1, 21, 17, 0, 0),
+    dueDate: today,
     description:
       'Empty dishwasher, load dirty dishes into dishwasher, rerun clean cycle',
   });
 
-  const listOfLists = {};
-  addTodos(listOfLists, inboxTodo1, inboxTodo2);
+  const inboxTodo3 = new Todo({
+    title: 'Fold laundry',
+    dueDate: tomorrow,
+  });
 
-  return { listOfLists, todos: [inboxTodo1, inboxTodo2] };
+  const listOfLists = {};
+  addTodos(listOfLists, inboxTodo1, inboxTodo2, inboxTodo3);
+
+  return { listOfLists, todos: [inboxTodo1, inboxTodo2, inboxTodo3] };
 }
